0b258394cb6dbcdb073ff407588b40aac05d56cc
Adriano Driuzzo <adriano.driuzzo@mzgroup.com>
2022-09-06 19:53:22 -0300

    Configure cypress grep

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,11 +17,5 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
-
-
-module.exports = (on, config) => {
   require('cypress-grep/src/plugin')(config)
 }
